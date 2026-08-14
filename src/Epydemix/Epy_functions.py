eeb371a560082c1b29e7f68a896189210c6ae5f4
Gabi <19386928@brookes.ac.uk>
2026-08-14 11:35:23 +0100

    Summary statistics function

--- src/Epydemix/Epy_functions.py
+++ src/Epydemix/Epy_functions.py
@@ -71,3 +71,23 @@ def stochastic_sir_noisy(beta, gamma, N, n_i, timesteps, rho, phi):
         noisy.append(rvs)
 
     return {"S": S_arr, "I": I_arr, "R": R_arr, "incidence": incidence, "noisy": noisy}
+
+
+# Summary statistics of posterior distribution
+def summary(param_1, param_2, posterior_1, posterior_2, decimal):
+    summary = {'Parameter': [param_1, param_2],
+           'Mean': [az.mean(posterior_1, round_to=decimal), az.mean(posterior_2, round_to=decimal)],
+           'Variance': [round(statistics.variance(posterior_1.flatten()),decimal), round(statistics.variance(posterior_2.flatten()),decimal)],
+           'Std': [az.std(posterior_1, round_to=decimal), az.std(posterior_2, round_to=decimal)],
+           'eti95_lb': [round(az.eti(posterior_1.flatten())[0],decimal), round(az.eti(posterior_2.flatten())[0],decimal)],
+           'eti95_ub': [round(az.eti(posterior_1.flatten())[1],decimal), round(az.eti(posterior_2.flatten())[1],decimal)],
+           'hdi95_lb': [round(az.hdi(posterior_1.flatten())[0],decimal), round(az.hdi(posterior_2.flatten())[0],decimal)],
+           'hdi95_ub': [round(az.hdi(posterior_1.flatten())[1],decimal), round(az.hdi(posterior_2.flatten())[1],decimal)],
+           'ess_bulk': [round(az.ess(posterior_1, method='bulk').item(),decimal), round(az.ess(posterior_2, method='bulk').item(),decimal)],
+           'ess_tail': [round(az.ess(posterior_1, method='tail', prob=0.95).item(),decimal), round(az.ess(posterior_2, method='tail', prob=0.95).item(),decimal)],
+           'rhat': [round(az.rhat(posterior_1).item(),decimal), round(az.rhat(posterior_2).item(),decimal)],
+           'mcse_mean': [round(az.mcse(posterior_1, method='mean').item(),decimal), round(az.mcse(posterior_2, method='mean').item(),decimal)],
+           'mcse_std': [round(az.mcse(posterior_1, method='sd').item(),decimal), round(az.mcse(posterior_2, method='sd').item(),decimal)]
+           }
+    df_summary = pd.DataFrame(summary)
+    return display(df_summary)
