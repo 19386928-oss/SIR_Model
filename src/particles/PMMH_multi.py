61654c187cd7773c490437a68c83621bf2b238b7
Gabi <19386928@brookes.ac.uk>
2026-09-25 09:57:45 +0100

    Prior distributions

--- src/particles/PMMH_multi.py
+++ src/particles/PMMH_multi.py
@@ -11,9 +11,10 @@ import os
 from particles import distributions as dists
 from particles import mcmc
 
+
 # Prior distributions for Parameters
-prior_dict = {'beta': dists.Uniform(a=0, b=0.5),
-              'gamma': dists.Uniform(a= 0, b= 0.5)}  # Uniform distribution because the prior is uninformative
+prior_dict = {'beta': dists.Gamma(a = 4.5, b = 10),
+              'gamma': dists.Gamma(a = 8, b = 100)}  # Uniform distribution because the prior is uninformative
 
 my_prior = ScalarStructDist(prior_dict)      # need this because 'dict' object has no attribute 'dtype' but StructDist does
 
@@ -23,22 +24,23 @@ def pmmh_multi(seed):
     pmmh_multi = mcmc.PMMH(ssm_cls= ChainBinomialModel, 
                      prior = my_prior, 
                      data=y, 
-                     Nx=10000, 
+                     Nx=1000, 
                      niter=5000, 
                      adaptive=True,
+                     smc_options= {'resampling': 'systematic', 'verbose': 'True'}
                     )
     pmmh_multi.run()
-    return pmmh_multi.chain
+    return pmmh_multi
 
 start = time.time()
 with mp.Pool() as pool:
-    pool_pmcmc = pool.map(pmmh_multi, [1,2,3,4])
+    pool_pmcmc = pool.map(pmmh_multi, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 end = time.time()
 print(f"Time taken: {end - start:.2f} seconds")
 
 # Save PMMH object
-with open('pmmh_chain_multi.pkl', 'wb') as output_multi:
+with open('pmmh_test_cpu.pkl', 'wb') as output_multi:
     pickle.dump(pool_pmcmc, output_multi)
 
 print("Chain saved.")
-print(os.path.abspath("pmmh_multi_chain.pkl"))
+print(os.path.abspath("pmmh_test_cpu.pkl"))
